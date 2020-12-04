d9f86c074b19f296055fd7abaabc41690e53fa60
DimK10 <james_k23@hotmail.gr>
2020-12-05 00:02:39 +0200

    Progress card state chenges complete. Only need to add async call with the changes to api

--- assets/js/kanBanBoard.js
+++ assets/js/kanBanBoard.js
@@ -81,8 +81,7 @@ $(function () {
 
         let $initializedProgressCard = $(this).closest(".progress__card");
 
-        // take the data from the input
-        $initializedProgressCard.find(".fa-times-circle").replaceWith("<i class='fas fa-trash-alt'></i>")
+
 
         // TODO -SANITIZE
         let $inputText = $initializedProgressCard.find("input").val();
@@ -91,6 +90,13 @@ $(function () {
         let $descpriptionBeforeInitialization = $initializedProgressCard.find("textarea").val();
 
         if ($inputText !== "" && $descpriptionBeforeInitialization !== "") {
+
+            // Replace check icon with edit icon
+            $initializedProgressCard.find(".fa-check").replaceWith("<i class='fas fa-pencil-alt'></i>")
+
+            // take the data from the input
+            $initializedProgressCard.find(".fa-times-circle").replaceWith("<i class='fas fa-trash-alt'></i>")
+
             // Replace input with a span element
             $initializedProgressCard.find(".input-group.progress__card--input-text input").replaceWith($(document.createElement("span")).text($inputText))
             $initializedProgressCard.find(".input-group.progress__card--input-text").attr("class", "progress__card--text")
@@ -104,9 +110,6 @@ $(function () {
         } else {
             // User is trying to create an empty card - warn him
             $initializedProgressCard.find('#tooltip').show().animate({ opacity: 1 }, 500);
-
-
-
         }
 
 
@@ -125,8 +128,9 @@ $(function () {
         // Get the values from the progress__card
         let $editedProgressCard = $(this).closest(".progress__card");
 
+        $editedProgressCard.find(".fa-pencil-alt").replaceWith(" <i class='fa fa-check'></i>")
+
         // Change delete icon to stop edit
-        console.log($(this).parent().find(".fa-trash-alt"))
         $editedProgressCard.find(".fa-trash-alt").replaceWith("<i class='far fa-times-circle'></i>")
 
         // TODO -SANITIZE!!!
@@ -159,22 +163,35 @@ $(function () {
         let $editedProgressCard = $(this).closest(".progress__card");
 
 
-        $editedProgressCard.find(".fa-times-circle").replaceWith("<i class='fas fa-trash-alt'></i>")
+        // Check if title and description are not empty and then modify
 
-        let $inputText = $editedProgressCard.find("input").val();
+        let $progressCardTitle = $editedProgressCard.find("input").val();
+
+        let $progressCardDesc = $editedProgressCard.find("textarea").val();
+
+        if ($progressCardTitle !== "" && $progressCardDesc !== "") {
+            $editedProgressCard.find(".fa-times-circle").replaceWith("<i class='fas fa-trash-alt'></i>")
+
+            let $inputText = $editedProgressCard.find("input").val();
 
 
 
-        // Replace input with a span element
-        $editedProgressCard.find(".input-group.progress__card--input-text input").replaceWith($(document.createElement("span")).text($inputText))
-        $editedProgressCard.find(".input-group.progress__card--input-text").attr("class", "progress__card--text")
+            // Replace input with a span element
+            $editedProgressCard.find(".input-group.progress__card--input-text input").replaceWith($(document.createElement("span")).text($inputText))
+            $editedProgressCard.find(".input-group.progress__card--input-text").attr("class", "progress__card--text")
 
-        // Grab any text that already existed in textarea
-        let $descpriptionBeforeEdit = $editedProgressCard.find("textarea").val();
+            // Grab any text that already existed in textarea
+            let $descpriptionBeforeEdit = $editedProgressCard.find("textarea").val();
 
-        // reset to p element
-        // todo sanitize
-        $editedProgressCard.find(".form-group.progress__card--textarea-text").replaceWith($(document.createElement("p")).text($descpriptionBeforeEdit));
+            // reset to p element
+            // todo sanitize
+            $editedProgressCard.find(".form-group.progress__card--textarea-text").replaceWith($(document.createElement("p")).text($descpriptionBeforeEdit));
+
+
+        } else {
+            // show tooltip
+            $editedProgressCard.find('#tooltip').show().animate({ opacity: 1 }, 500);
+        }
 
 
 
